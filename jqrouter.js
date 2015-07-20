60a7d014e2a8e6f75c3082bc8d389b2ab8a6db8e
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-07-20 22:48:58 +0530

    JQ router Revamped

--- jqrouter.js
+++ jqrouter.js
@@ -1,20 +1,18 @@
-registerModule(this,'jqrouter', function(jqrouter, _jqrouter_){
+console.info("definig router")
+_define_('jqrouter', function(jqrouter){
 	
-	jqrouter._instance_ = function(){
-		this.ids = [];
-		this.$matched = false;
-	};
-	var _jqrouter_ = _jqrouter_ || jqrouter._instance_.prototype;
+	var JQROUTER = {};
 	
 	var otherWise = true, $matched = false;
-	var pathname, hash, contextPath = "/",hashData = {},counter=0, intialized = false,otherwiseURL;
+	var pathname, hash, queryString, contextPath = "/",hashData = {},counter=0, intialized = false,otherwiseURL;
 	var HASH_PARAM_PREFIX = "#&";
-	jqrouter.dead = {};
-	jqrouter.hashchange = function(){
+	JQROUTER.dead = {};
+	var hashchange = function(){
 		var _hashChange = (hash != document.location.hash);
 		var _pathChange = (pathname != document.location.pathname &&
 				(true || (document.location.pathname.length>0 && document.location.pathname.indexOf(pathname)!=0))
 		);
+		var _queryChange = (queryString!=document.location.search);
 		otherWise = true;
 		/*
 		 * Need to change URL first as it may trigger twice in between
@@ -25,88 +23,38 @@ registerModule(this,'jqrouter', function(jqrouter, _jqrouter_){
 		if (_pathChange) {
 			pathname = document.location.pathname;
 		}
+		if (_queryChange) {
+			queryString = document.location.search;
+		}
 		if(_hashChange){
-			this.invoke(hash);
+			JQROUTER.invoke(hash);
 		}
 		if(_pathChange){
-			this.invoke(pathname.replace(contextPath,"/"));
+			console.log("_pathChange",pathname);
+			JQROUTER.invoke(pathname.replace(contextPath,"/"));
 		}
+		if(_queryChange){
+			console.log("_queryChange",queryString);
+			JQROUTER.SET_PARAMS(URI.decode(queryString.slice(1)));
+		}
+		console.log("wee");
 	};
 	
-	jqrouter.cache = {};
-	jqrouter.onchange_map = {};
-	jqrouter.refineKey = function(_key){
+	var refineKey = function(_key){
 		//(contextPath + _key).replace(/[\/]+/g,'/')
-		return (jqrouter.cleanUrl(_key+"/")).replace(/\{(.*?)\}/gi,'$').replace("*","$");
+		return (URI.clean(_key+"/")).replace(/\{(.*?)\}/gi,'$').replace("*","$");
 		return _key;// .replace(/\[/gi, '#').replace(/\]/gi, '');
 	};
-	jqrouter.split = function(key){
+	
+	var splitURL = function(key){
 		return key.split(/[\/]+/gi);
 	};
-	jqrouter.invoke = function(_key,args){
-		if(_key.indexOf("#?") === 0){
-			jqrouter.setKeys(jqrouter.decode(_key.replace("#?","")));
-		} else {
-			var key = this.refineKey(_key);
-			return this.callFun(key,args);
-		}
-	};
 	
-	_jqrouter_.on = function(__key, fun, isHTTP){
-		var __keys = (__key.indexOf("*")>-1) ? [__key.replace("*","{x}"),__key.replace("*","")] : [__key];
-		var isHASH = __key.indexOf("#")>-1
-		for(var _i in __keys){
-			var _key = __keys[_i];
-			var key = jqrouter.refineKey(_key);
-			var keys = jqrouter.split(key);
-			var ref = jqrouter.onchange_fun;
-			var _nextKey = keys[0];
-			var _key = keys[0];
-			for ( var i = 0; i < keys.length ; i++) {
-				_key = keys[i];
-				_nextKey = keys[i + 1];
-				_atKey = '@' + _key;
-				ref[_atKey] = ref[_atKey] || {
-						fun : [], ids : [],
-						key : _key, nextKey : _nextKey, next : jqrouter.next
-					};
-				ref = ref[_atKey];
-			}
-			var id = counter++;
-			ref.fun.push({ cb : fun, id : id, url : __keys[_i] });
-			if(intialized){
-				jqrouter._callFun(
-						isHASH ? jqrouter.refineKey(document.location.hash) :
-						jqrouter.refineKey(document.location.pathname).replace(contextPath,"/"),id);
-				this.$matched = this.$matched || $matched;
-			}
-			this.ids.push(id);			
-		}
-		return this;
-	};
-	jqrouter.on = function(_key, fun, isHTTP){
-		var self = jqrouter.instance ? jqrouter.instance() : new jqrouter._instance_();
-		return self.on(_key, fun, isHTTP);
-	};
+	JQROUTER.onchange_map = {};
 
-	_jqrouter_.off = function(){
-		for(var i in this.ids){
-			jqrouter.dead[this.ids[i]] = true;
-		}
-	};
-	
-	_jqrouter_.otherwise = function(goToURL){
-		if(intialized && (otherWise || !this.$matched)){
-			jqrouter.go(goToURL);
-		} else if(!intialized){
-			otherwiseURL = goToURL;
-		}
-		return this
-	};
-	
 	// execute event handler
-	jqrouter._callFun = function(key,id,args){
-		var keys = jqrouter.split(key);
+	JQROUTER._callFun = function(key,id,args){
+		var keys = splitURL(key);
 		$matched = false;
 		if (this.onchange_fun.next) {
 			return this.onchange_fun.next(0,{
@@ -116,7 +64,7 @@ registerModule(this,'jqrouter', function(jqrouter, _jqrouter_){
 		}
 	};
 
-	jqrouter.next = function(index,o){
+	JQROUTER.next = function(index,o){
 		var curIndex = o.index++;
 		if (this['@' + o.keys[index]]) {
 			this['@' + o.keys[index]].next(index+1,o);
@@ -128,59 +76,120 @@ registerModule(this,'jqrouter', function(jqrouter, _jqrouter_){
 		if(index==o.keys.length){
 			for(var j in this.fun){
 				if(this.fun[j] && (o.id===undefined || o.id === this.fun[j].id)){
-					if(jqrouter.dead[this.fun[j].id]){
-						delete jqrouter.dead[this.fun[j].id];
+					if(JQROUTER.dead[this.fun[j].id]){
+						delete JQROUTER.dead[this.fun[j].id];
 						delete this.fun[j];
 					} else if(typeof this.fun[j].cb === 'function'){
 						otherWise = false;
 						$matched = true;
-						this.fun[j].cb.apply(jqrouter,o.arg.concat([o.extraArg]));
+						this.fun[j].cb.apply(JQROUTER,o.arg.concat([o.extraArg]));
 					}
 				}
 			}
 		}
 		return true;
 	};
-	jqrouter.onchange_fun = {
-		next : jqrouter.next,
+	JQROUTER.onchange_fun = {
+		next : JQROUTER.next,
 	};
 	
-	jqrouter.TRIGGER = debounce(function(arg){
+	JQROUTER.TRIGGER = debounce(function(arg){
 		intialized = true;
-		jqrouter.trigger(arg);
+		JQROUTER.trigger(arg);
 		if(!$matched && otherwiseURL){
 			var _otherwiseURL = otherwiseURL; otherwiseURL = null;
-			jqrouter.go(_otherwiseURL);
+			JQROUTER.GO(_otherwiseURL);
 		}
 	});
 	
 	// Registers an event to be triggered
-	jqrouter.callFun = function(key,arg){
+	JQROUTER.callFun = function(key,arg){
 		this.onchange_map[key] = true;
 		// return this._callFun(key);
-		jqrouter.TRIGGER(arg);
+		JQROUTER.TRIGGER(arg);
 	};
 	
 	// process event queue
-	jqrouter.trigger = function(arg){
+	JQROUTER.trigger = function(arg){
 		for ( var key in this.onchange_map) {
 			var propagation = this._callFun(key,undefined,arg);
 			delete this.onchange_map[key]
 		}
 	};
-	jqrouter.cleanUrl = function(url){
-		return url.replace(/[\/]+/g,'/');
+	
+	var isChanged = function(key,value){
+		return (JSON.stringify(hashData[key]) !== JSON.stringify(value));
 	};
-	jqrouter.go = function(url){
-		var goURL = ((url+"").indexOf("#") === 0) ? url : jqrouter.cleanUrl(contextPath + url);
+	
+	JQROUTER._config_ = function(moduleConfig,appConfig){
+		contextPath = appConfig.contextPath;
+	};
+	
+	JQROUTER.invoke = function(_key,args){
+		console.warn("invoke",arguments);
+		if(_key.indexOf("?") === 0){
+			JQROUTER.SET_PARAMS(URI.decode(_key.slice(1)));
+		} else {
+			var key = refineKey(_key);
+			return this.callFun(key,args);
+		}
+	};
+	
+	var paramEvents = {};
+	JQROUTER.CALL_PARAM_CHANGE = debounce(function(keyname){
+		for(var i in paramEvents){
+			if(paramEvents[i] && paramEvents[i].name == keyname && is.Function(paramEvents[i].fun)){
+				paramEvents[i].fun(keyname,hashData[keyname]);
+			}
+		}
+	});
+	
+	//Globals Functions
+	JQROUTER.GO = debounce(function(url){
+		console.warn("GO",arguments);
+		var _url = url+"";
+		var goURL = (_url.indexOf("#") === 0) ? url : ((_url.indexOf("?") === 0) ? (pathname+_url) : URI.clean(contextPath + url));
 		return window.history.pushState(null,null,goURL);
+	});
+	
+	JQROUTER.REOLOAD = function(url){
+		window.location.href = (url!==undefined)?  URI.clean(contextPath + url) : window.location.href;
 	};
-	jqrouter.reload = function(url){
-		window.location.href = (url!==undefined)?  jqrouter.cleanUrl(contextPath + url) : window.location.href;
+	JQROUTER.SET_PARAM = function(key,value){
+		console.warn("SET_PARAM",arguments);
+		if(isChanged(key,value)){
+			hashData[key] = JSON.parse(JSON.stringify(value));
+			JQROUTER.CALL_PARAM_CHANGE(key)
+			JQROUTER.GO("?"+URI.encode(hashData));
+			return true;
+		} return false;
 	};
-	jqrouter._ready_ = function(){
+	JQROUTER.GET_PARAM = function(key,defValue){
+		return hashData[key] ===undefined ? defValue : JSON.parse(JSON.stringify(hashData[key]));
+	};
+	JQROUTER.GET_PARAMS = function(keyMap){
+		var retMap = {};
+		for(var key in keyMap){
+			retMap[key] = JQROUTER.GET_PARAM(key,keyMap[key]);
+		}
+		return retMap;
+	};
+	JQROUTER.SET_PARAMS = function(newHashData){
+		console.warn("SET_PARAMs",arguments);
+		hashData = {};
+		for(var key in newHashData){
+			if(isChanged(key,newHashData[key])){
+				hashData[key] = JSON.parse(JSON.stringify(newHashData[key]));
+				JQROUTER.CALL_PARAM_CHANGE(key)
+			}
+		}
+		JQROUTER.GO("?"+URI.encode(hashData));
+	};
+	
+	console.warn("check");
+	when.ready(function(event) {
 	    var pushState = history.pushState;
-	    
+	    console.warn("wee");
 	    history.pushState = function(state) {
 	        if (typeof history.onpushstate == "function") {
 	           // history.onpushstate({state: state});
@@ -191,110 +200,96 @@ registerModule(this,'jqrouter', function(jqrouter, _jqrouter_){
 	        } else {
 	        	ret = pushState.apply(history, arguments);
 	        }
-	        jqrouter.hashchange();
+	        hashchange();
 	        return ret;
-	    }
+	    };
 		window.onpopstate = history.onpushstate = function(e) {
-			jqrouter.hashchange();
-		}
-		$('body').on('click','a.jqrouter,a[jqrouter]', function(e){
-			var href = this.getAttribute('jqrouter') || this.getAttribute('href');
-			if(!jqrouter.isRemote(href) && !e.ctrlKey){
-				if(href.indexOf(HASH_PARAM_PREFIX) === 0){
-					var params = href.replace(HASH_PARAM_PREFIX,"").split("=");
-					if(jqrouter.setKey.apply(jqrouter,params)){
-						$(this).trigger("jqrouter#&"+params[0],{key : params[0], value : params[1]});
-						//jqrouter.invoke("#&"+params[0]+"=",params[1]);
-					}
-					//jqrouter.go("#?"+jqrouter.encode(hashData));
-					return preventPropagation(e)
-				} else if(href.indexOf("#") === 0){
-					//jqrouter.go(href.replace(contextPath,"/"));
-				} else {
-					jqrouter.go(href.replace(contextPath,"/"));
-					return preventPropagation(e)
+			hashchange();
+		};
+		hashchange();
+	});
+	
+	//APIS Starts from Here
+	return {
+		ids : [],
+		paramEventIds : [],
+		$matched : false,
+		_instance_ : function(){
+			this.ids = [];
+			this.$matched = false;
+		},
+		instance : function(){
+			var ins = Object.create(this);
+			ins._instance_.apply(ins,arguments)
+			return ins;
+		},
+		on : function(__key, fun, isHTTP){
+			var __keys = (__key.indexOf("*")>-1) ? [__key.replace("*","{x}"),__key.replace("*","")] : [__key];
+			var isHASH = __key.indexOf("#")>-1
+			var isQUERY = __key.indexOf("?")==0;
+			if(isQUERY){
+				var query = URI.decode(__key.slice(1));
+				for(var key in query){
+					var id = counter++;
+					this.paramEventIds.push(id);
+					paramEvents[id] = {
+						name : key, fun : fun
+					};
 				}
+			} else {
+				for(var _i in __keys){
+					var _key = __keys[_i];
+					var key = refineKey(_key);
+					var keys = splitURL(key);
+					var ref = JQROUTER.onchange_fun;
+					var _nextKey = keys[0];
+					var _key = keys[0];
+					for ( var i = 0; i < keys.length ; i++) {
+						_key = keys[i];
+						_nextKey = keys[i + 1];
+						_atKey = '@' + _key;
+						ref[_atKey] = ref[_atKey] || {
+								fun : [], ids : [],
+								key : _key, nextKey : _nextKey, next : JQROUTER.next
+							};
+						ref = ref[_atKey];
+					}
+					var id = counter++;
+					ref.fun.push({ cb : fun, id : id, url : __keys[_i] });
+					if(intialized){
+						JQROUTER._callFun(
+								isHASH ? refineKey(document.location.hash) :
+								refineKey(document.location.pathname).replace(contextPath,"/"),id);
+						this.$matched = this.$matched || $matched;
+					}
+					this.ids.push(id);			
+				}
+	
 			}
-		});
-		jqrouter.hashchange();
-	};
-	
-	var isChanged = function(key,value){
-		return (JSON.stringify(hashData[key]) !== JSON.stringify(value));
-	};
-	
-	jqrouter.setKey = function(key,value){
-		if(isChanged(key,value)){
-			hashData[key] = JSON.parse(JSON.stringify(value));
-			jqrouter.go("#?"+jqrouter.encode(hashData));
-			jqrouter.invoke("#&"+key+"=",value);
-			return true
-		} return false;
-	};
-	
-	jqrouter.getKey = function(key,defValue){
-		return hashData[key] ===undefined ? defValue : JSON.parse(JSON.stringify(hashData[key]));
-	};
-	
-	jqrouter.getKeys = function(keyMap){
-		var retMap = {};
-		for(var key in keyMap){
-			retMap[key] = jqrouter.getKey(key,keyMap[key])
-		}
-		return retMap;
-	};
-	jqrouter.setKeys = function(newHashData){
-		for(var key in newHashData){
-			if(isChanged(key,newHashData[key])){
-				hashData[key] = JSON.parse(JSON.stringify(newHashData[key]));
-				jqrouter.invoke("#&"+key+"=",newHashData[key])
+			return this;
+		},
+		off : function(){
+			for(var i in this.ids){
+				JQROUTER.dead[this.ids[i]] = true;
 			}
-		}
-		jqrouter.go("#?"+jqrouter.encode(hashData));
-	};
-	jqrouter.encode = function(param){
-		return $.param(param);
-		return encode64(JSON.stringify(hashData))
+			for(var i in this.paramEventIds){
+				delete paramEvents[this.paramEventIds[i]];
+			}
+		},
+		otherwise : function(goToURL){
+			if(intialized && (otherWise || !this.$matched)){
+				JQROUTER.GO(goToURL);
+			} else if(!intialized){
+				otherwiseURL = goToURL;
+			}
+			return this
+		},
+		go : JQROUTER.GO,
+		reload : JQROUTER.REOLOAD,
+		getParam : JQROUTER.GET_PARAM,
+		setParam : JQROUTER.SET_PARAM,
+		getParams : JQROUTER.GET_PARAMS,
+		setParams : JQROUTER.SET_PARAMS
 	};
 	
-	jqrouter.decode = function (p) {
-	    var params = {};
-	    var pairs = p.split('&');
-	    for (var i=0; i<pairs.length; i++) {
-	    	if(pairs[i]){
-	    		var pair = pairs[i].split('=');
-		        var accessors = [];
-		        var name = decodeURIComponent(pair[0]), value = decodeURIComponent(pair[1]);
-
-		        var name = name.replace(/\[([^\]]*)\]/g, function(k, acc) { accessors.push(acc); return ""; });
-		        accessors.unshift(name);
-		        var o = params;
-
-		        for (var j=0; j<accessors.length-1; j++) {
-		            var acc = accessors[j];
-		            var nextAcc = accessors[j+1];
-		            if (!o[acc]) {
-		                if ((nextAcc == "") || (/^[0-9]+$/.test(nextAcc)))
-		                    o[acc] = [];
-		                else
-		                    o[acc] = {};
-		            }
-		            o = o[acc];
-		        }
-		        acc = accessors[accessors.length-1];
-		        if (acc == "")
-		            o.push(value);
-		        else
-		            o[acc] = value;
-	    	}
-	    }
-	    return params;
-	    return JSON.parse(decode64(value));
-	};
-	jqrouter.isRemote = function(path){
-		return  (path.indexOf('http://')==0 || path.indexOf('https://')==0)
-	};
-	jqrouter._config_ = function(moduleConfig,appConfig){
-		contextPath = appConfig.contextPath;
-	};
 });
